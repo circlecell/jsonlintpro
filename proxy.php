a0922f4db04cb9cc88e12307428353653ff97966
Mike Ciarlo <mbciarlo@gmail.com>
2012-12-19 18:11:40 +0000

    Removing old files.

--- proxy.php
+++
@@ -1,3 +0,0 @@
-
-Notice: Undefined index: url in /srv/www/jsonlint/proxy.php on line 3
-{ "result": "Invalid URL. Please check your URL and try again.", "error": true }{ "result": "Unable to parse URL. Please check your URL and try again.", "error": true }
